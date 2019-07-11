8f764c015a14eb12b6fe2c03646c498fadafa7ff
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2019-07-11 23:04:43 +0200

    wrapper function for malloc implemented; gen_time_stamp function with daylight saving time control implemented;

---
+++ src/header/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/param.h>
+#include <openssl/sha.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define BUFFLEN 256
+#define TIME_STAMP_LEN 25
+
+void* obj_malloc(size_t size);
+char *gen_time_stamp();
+
+#endif
---
+++ src/lib/utils.c
@@ -0,0 +1,43 @@
+#include "../header/utils.h"
+
+
+void *obj_malloc(size_t size)
+{
+  void *ptr;
+  ptr  = malloc(size);
+  memset(ptr, 0, size);
+  if(ptr == NULL)
+  {
+    perror("Failed to allocate object");
+    exit(EXIT_FAILURE);
+  }
+  return ptr;
+}
+
+
+char *gen_time_stamp()
+{
+  char *time_stamp = (char*)obj_malloc(sizeof(char) * TIME_STAMP_LEN);
+
+  time_t timeval = time(NULL);
+  struct tm *t = gmtime(&timeval);
+
+  printf("\n\nvalore del flag :%d\n\n", t->tm_isdst);
+
+
+  // if daylight saving time is in effect
+  if (t->tm_isdst == 0)
+    sprintf(time_stamp, "%d/%d/%d_%d:%d:%d", \
+      t->tm_year + 1900, t->tm_mon, t->tm_mday, \
+      // +1 for daylight saving time and +1 for the italian time zone
+      t->tm_hour + 2, \
+      t->tm_min, t->tm_sec);
+  else
+    sprintf(time_stamp, "%d/%d/%d_%d:%d:%d", \
+      t->tm_year + 1900, t->tm_mon, t->tm_mday, \
+      // +1 for the italian time zone
+      t->tm_hour + 1, \
+      t->tm_min, t->tm_sec);
+
+  return time_stamp;
+}
