ed697dd4681d6fbe943f499d418b20d371fd0751
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2019-07-12 16:58:57 +0200

    hash.h implementation

---
+++ src/header/hash.h
@@ -0,0 +1,25 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/param.h>
+#include <openssl/sha.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "utils.h" // for obj_malloc & BUFFLEN def
+
+typedef unsigned char* hash_t;
+
+// calculate hash for the string provided
+hash_t calculate_hash(char s[BUFFLEN]);
+// hexadecimal hash print
+void print_hash(hash_t h);
+// strcpy wrapper for assign hash to another
+void hashcpy(hash_t dst, hash_t src);
+// strncmp wrapper for compare hash with another
+bool hash_equal(hash_t dst, hash_t src);
+
+#endif
---
+++ src/lib/hash.c
@@ -0,0 +1,31 @@
+#include "../header/hash.h"
+
+hash_t calculate_hash(char s[BUFFLEN])
+{
+  hash_t hash = (hash_t)obj_malloc(SHA256_DIGEST_LENGTH);
+  SHA256((hash_t)s, strlen(s), hash);
+  return hash;
+}
+
+
+void print_hash(hash_t h)
+{
+  printf("\n");
+  for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+  {
+    printf("%02x", h[i]);
+  }
+  printf("\n");
+}
+
+
+void hashcpy(hash_t dst, hash_t src)
+{
+  strncpy((char*)dst, (const char *restrict)src, SHA256_DIGEST_LENGTH);
+}
+
+
+bool hash_equal(hash_t a, hash_t b)
+{
+  return (strncmp((const char *)a, (const char *)b, SHA256_DIGEST_LENGTH) == 0) ? true : false;
+}
