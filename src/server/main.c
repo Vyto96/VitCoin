ae9c0b3deaccb01c4eef6a03a592669a5fa72bc8
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2019-08-04 00:21:40 +0200

    skeleton of the server code, implemented

---
+++ src/server/main.c
@@ -0,0 +1,86 @@
+#include "server.h"
+
+int main(int argc, char **argv)
+{
+  srand(time(NULL));
+
+  //------------------------------------------------------------------------VAR
+  struct sockaddr_in server_add;
+  int list_fd;
+  unsignd short listen_port;
+
+  //-------------------------------------------------------------INIT GLOBAL VAR
+  read_cli_param(argc, argv, listen_port);
+  network = create_list();
+
+  //----------------------------------------------------------------SETUP SERVER
+  fill_address(&server_add, AF_INERT, NULL, listen_port);
+  // listening on tcp socket type
+  list_fd = Socket(AF_INET, SOCK_STREAM, 0);
+  // set reuse address option on socket
+  int optval = 1;
+  setsockopt(list_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval) );
+  // Bind of address
+  Bind(list_fd, (struct sockaddr *) &server_add);
+  // set the backlog for listen queue
+  Listen(list_fd, BACKLOG);
+
+  int request, authorizzation = 0;
+  hash_t client_psw = (hash_t)obj_malloc(SHA256_DIGEST_LENGTH);
+
+  While(1)
+  {
+    print_status();
+    client_fd = Accept(list_fd, NULL);
+
+    //------------------------------------------------------------CHECK PASSWORD
+    full_read(client_fd, client_psw, SHA256_DIGEST_LENGTH);
+    if( !hash_equal(hash_psw, client_psw) ) // not authenticated
+    {
+      authorizzation = 0;
+      send_short(client_fd, authorizzation); //send 0, refusing
+      printf("Rejecting request, password is NOT correct.\n");
+    }
+    else //authenticated
+    {
+      // sending 1, accepting
+      authorizzation = 1;
+      send_short(client_fd, authorizzation);
+
+      // waiting for request (MACROs)
+      recv_short(client_fd, &request);
+
+      printf("\nSHA256 verified, serving request of type: ");
+      switch (request)
+      {
+        case HOOK_PEER:
+          printf("HOOK_PEER\n");
+          hook_peer();
+          break;
+
+        case RE_HOOK_PEER:
+          printf("RE_HOOK_PEER\n");
+          re_hook_peer();
+          break;
+
+        case HOOK_WALLET:
+          printf("HOOK_WALLET\n");
+          hook_wallet();
+          break;
+
+        case RE_HOOK_WALLET:
+          printf("RE_HOOK_WALLET\n");
+          re_hook_wallet();
+          break;
+
+        default:
+          printf("MACRO not correct\n");
+          printf("Aborting.\n");
+          break;
+      }
+    }
+    close(client_fd);
+  }
+
+  exit(EXIT_SUCCESS);
+}
---
+++ src/server/server.h
@@ -0,0 +1,43 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+
+#include "../header/p2p.h"
+#include "../header/list.h"
+#include "../header/net.h"
+#include "../header/hash.h"
+#include "../header/fullio.h"
+#include "../header/utils.h"
+#include "../header/sockwrap.h"
+
+
+#define SERVER_USAGE "Usage: ./server \n \
+    \t[-l LISTEN_PORT || default = 7777]\n \
+    \t[-p PASSWORD || default = \"VitCoin\"]\n \
+    \t-h HELP: display this usage\n"
+
+#define DEFAULT_PSW "VitCoin"
+#define DEFAULT_SERVER_PORT 7777
+#define MIN_PEERS_PERC 50/100
+
+//--------------------------------------------------------------------GLOBAL VAR
+List network; // list of peers service Net_ent
+// struct s_net_ent client_ent; // TODO: CONTROLLA SE SERVE
+hash_t hash_psw // password for for access to network
+int client_fd; // used from every functions
+
+// REQUEST FUNCTIONS
+void hook_peer();
+// void re_hook_peer();
+// void hook_wallet();
+// void re_hook_wallet();
+
+// UTILITY
+void read_cli_param(int argc, char **argv, int* port);
+void print_status();
+#endif
