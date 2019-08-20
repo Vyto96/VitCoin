e5aac3c7c056f7e6d49605713254f3136c3101ef
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2019-08-20 23:02:04 +0200

    final

--- src/peer/peer.h
+++ src/peer/peer.h
@@ -27,67 +27,108 @@
 #include "peer_pkg.h"
 
 #define PEER_USAGE "Usage: ./peer \n \
-      \t[-a CENTRAL_SERVER_IP_ADDRESS || 127.0.0.1]\n \
-      \t[-b CENTRAL_SERVER_PORT || 7777]\n \
-      \t[-p PASSWORD || \"VitCoin\"]\n \
+      \t[-a CENTRAL_SERVER_IP_ADDRESS || default = 127.0.0.1]\n \
+      \t[-b CENTRAL_SERVER_PORT || default = 7777]\n \
+      \t[-p PASSWORD || default = \"VitCoin\"]\n \
       \t-s SERVICE_PEER_PORT\n\
-      \t[-t TEST_MODE: create a fake multi-tail blockchain]\n\
+      \t[-t TEST_MODE_MINING_TIME]: create a fake multi-tail blockchain\n\
       \t-h HELP: display this usage\n"
 
-#define DEFAULT_SERVER_ADD "127.0.0.1"
-#define DEFAULT_SERVER_PORT 7777
-#define DEFAULT_PSW "VitCoin"
 
-//--------------------------------------------------------------------GLOBAL VAR
-struct sockaddr_in server_add;
-struct s_net_ent my_service_ent, server;
-bool test_mode;
-hash_t hash_psw; // password for for access to network
+// *****************************************************************************GLOBAL VAR
+struct s_net_ent my_service_ent, server_ent;
+short test_mode;
+hash_t hash_psw;
 
-// thread & their Sincro stuff
+// Threads & their Sincro stuff
 pthread_attr_t *attr;
 
 RW_sincro sincro_conn_peer;
 RW_sincro sincro_conn_wallet;
 RW_sincro sincro_block_chain;
-sem_t *printer_sem;
+sem_t *printer_sem, *checker_sem;
 
-//---------------------------------------------------MUTEX ACCESS
-// Blockchain stuff
+//------------------------------------------------------------------------------MUTEX ACCESS
+// -------------------------------------------------------------Blockchain stuff
 Bchain block_chain;
-int next_seq;
+
+// tid of the thread that is booked to insert in the blockchain
 pthread_t reserved_tid;
+
+// if set to true it means that some thread is trying to create/insert a block
 bool flag_tid;
 
-// connections stuff
+/*if set to true it means that it is necessary to check the tail_to_cut
+  list to check if it is possible to recreate an obsolete block*/
+bool check_ttc;
+
+// ------------------------------------------------------------Connections stuff
 List conn_peer;
 List conn_wallet;
 int max_fd;
 int fd_open[FD_SETSIZE];
 
-// signal stuff
+// flags used by signal handler function
 bool exit_flag, refresh_flag;
 
+// *****************************************************************************FUNCTIONS
 // ----------------------------------------------------------------------NETWORK
-/* request to hook to a peer. NULL is return if failled. Else it returns
-   the Connected_ent to add to conn_peer list */
+/* request to hook to a peer. NULL is return if failled.
+   Else it returns the Connected_ent to add to conn_peer list */
 Connected_ent hook_to_peer(struct s_net_ent peer_ent, short download);
 
+// returns true if the network connection protocol succesfull
 bool hook_network();
 
+
+// --------------------------------------------------------------USED BY THREADS
+// create  (or recreate, if bl_to_add is not null)  block in block_chain
+Block sync_in_blockchain(Trns tr, Block bl_to_add);
+
+// flooding of re/created block package
+void flooding(void* pkg, short pkg_type, struct s_net_ent pkg_sender);
+
+// function to warn a wallet in case he received a transaction
+void warn_wallet(Trns t);
+
+// used from "block_to_recreate_checker" thread
+void recreate_block();
+
+// used from "peer_state_printer" thread
+void print_peer_state();
+
+// thread refresh of pselect using a signal
+void refresh_state();
+
+/* used only in test_mode and by only first peer of network.
+   Create a fake blockchain with multi-tails for testing.*/
+void test_bchain();
 // ----------------------------------------------------REQUEST SERVED BY THREADS
 void *peer_state_printer();
-void *hook_p2p(void *arg);
+void *block_to_recreate_checker();
+
+// have a peer hooked up
+void *hook_p2p(void* arg);
+// have a wallet hooked up
+void *hook_w2p(void* arg);
+
+// create and "flood" the transaction received from the wallet
+void *w_transaction(void* arg);
+// add block received and "flood it"
+void *p_block(void* arg);
+// add recreated block received and "flood it"
+void *p_recreated_block(void* arg);
+// search for all wallet transactions in the blockchain
+void *w_balance(void* arg);
+
+// handle the error detected on a socket
+void *socket_error(void *arg);
 
 // ----------------------------------------------------------------------UTILITY
 void read_cli_param(int argc, char **argv);
 void init_global_var();
 void destroy_global_var();
 void sig_handler(int n);
-void print_peer_state();
-
-
-
-
+void delete_from_network();
 
 #endif
