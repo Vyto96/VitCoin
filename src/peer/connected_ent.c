e45dadd798c31bcb8df5c9dcfcc6c6066b18210d
Vyto <vittorio.zavino001@studenti.uniparthenope.it>
2019-08-20 23:04:09 +0200

    final

--- src/peer/connected_ent.c
+++ src/peer/connected_ent.c
@@ -20,6 +20,7 @@ bool compare_connected_ent_by_fd(void *x, void *y)
 {
   Connected_ent a = (Connected_ent)x;
   Connected_ent b = (Connected_ent)y;
+  
   if(a == NULL || b == NULL)
   {
     fprintf(stderr, "\nATTENTION: Connected_ent error, NULL pointer found in compare_connected_ent_by_fd()\n" );
--- src/peer/connected_ent.h
+++ src/peer/connected_ent.h
@@ -14,7 +14,7 @@ typedef struct s_connected_ent *Connected_ent;
 #define CONNECTED_ENT_SIZE sizeof(struct s_connected_ent)
 
 
-// I/O params void used for compatibility with list library
+// I/O params void* used for compatibility with list library
 void visit_connected_ent(void *arg);
 bool compare_connected_ent(void *x, void *y);
 bool compare_connected_ent_by_fd(void *x, void *y);
